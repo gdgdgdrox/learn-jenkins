7baa8f60575485a9b669341fabce047742697883
Foo Guo Dong <gd@Foos-MacBook-Pro.local>
2023-11-05 17:30:29 +0800

    init

---
+++ src/main/java/com/simpleapp/App.java
@@ -0,0 +1,13 @@
+package com.simpleapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is a simple app" );
+    }
+}
